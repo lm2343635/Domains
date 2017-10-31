bd0603cffb7ae6d3a6cbf51df0e399b8d5b9d480
limeng <lm2343635@126.com>
2017-10-31 19:49:09 +0900

    Add a new report.

--- src/main/webapp/static/js/employee/report.js
+++ src/main/webapp/static/js/employee/report.js
@@ -14,4 +14,33 @@ $(document).ready(function () {
         height: 600
     });
 
+    $("#report-save").click(function () {
+        var title = $("#report-title").val();
+        var content = $("#report-content").summernote("code");
+        if (title == "" || content == "") {
+            $.messager.popup("标题和内容不能为空！");
+            return;
+        }
+        $(this).text("提交中...").attr("disabled", "disabled");
+        if (rid != null && rid != "") {
+
+        } else {
+            ReportManager.add(title, content, function (result) {
+                if (!result.session) {
+                    sessionError();
+                    return;
+                }
+                if (result.data == null) {
+                    $.messager.popup("保存失败，请刷新重试！");
+                    return;
+                }
+                $("#report-save").text("保存报告").removeAttr("disabled");
+                $.messager.popup("保存成功！");
+                setTimeout(function () {
+                    location.href = "reports.html";
+                }, 1000);
+            });
+        }
+    });
+
 });
